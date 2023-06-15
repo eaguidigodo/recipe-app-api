097a532273c03134bdc60b05e9bc38322cf1fdd2
Enselme <eaguidigodo@gmail.com>
2023-06-15 10:04:26 +0100

    Django admin customization

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,32 @@
+"""
+Tests for  the Django admin modifications.
+"""
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Tests for Django admin."""
+
+    def setUp(self):
+        """Create user and client."""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@example.com',
+            password = 'adminpass123'
+        )
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'testuser@example.com',
+            password = 'testpass123',
+            name = 'Test User'
+        )
+    def test_users_list(self):
+        """Test that users are listed on page."""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res,self.user.email)
+        self.assertContains(res, self.user.name)
